3291e47242b390e27b77362eb573462b48cf8ce7
Boris Dinkevich <boris@dinkevich.com>
2014-11-06 14:57:24 +0200

    Added apps#index

--- client/config/routes.js
+++ client/config/routes.js
@@ -13,5 +13,15 @@ angular.module('app.routes', []).
       .state('sign_in', {
         url: '/sign_in',
         templateUrl: 'views/auth/sign_in.tpl.html'
+      })
+      .state('apps', {
+        url: '/apps',
+        abstract: true,
+        template: '<div ui-view></div>'
+      })
+      .state('apps.index', {
+        url: '',
+        controller: 'AppsIndexController',
+        templateUrl: 'views/apps/index.tpl.html'
       });
     });
