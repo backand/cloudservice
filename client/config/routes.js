916889dfd19f4f16843576e12206c768fcea0a0c
Boris Dinkevich <boris@dinkevich.com>
2014-11-06 14:47:06 +0200

    Added sign_in template

--- client/config/routes.js
+++ client/config/routes.js
@@ -4,18 +4,14 @@ angular.module('app.routes', []).
   config(function($stateProvider, $urlRouterProvider, $logProvider, $httpProvider) {
 
     $urlRouterProvider.otherwise('/');
+
     $logProvider.debugEnabled(true);
+
     $httpProvider.interceptors.push('httpInterceptor');
 
     $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'views/home/home.tpl.html',
-        controller: 'HomeCtrl as home',
-        resolve: {
-          data: function(DataService) {
-            return DataService.get();
-          }
-        }
+      .state('sign_in', {
+        url: '/sign_in',
+        templateUrl: 'views/auth/sign_in.tpl.html'
       });
     });
