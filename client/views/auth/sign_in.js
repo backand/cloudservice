3696aa01e66eb2e92d07b648ed06587d5900e156
nadav <nadav22@gmail.com>
2014-11-12 16:54:55 +0200

    fixing log in data

--- client/views/auth/sign_in.js
+++ client/views/auth/sign_in.js
@@ -2,9 +2,9 @@
 (function  () {
 
   angular.module('app')
-    .controller('SignInController',["$scope",'AuthService','$state','SessionService',SignInController]);
+    .controller('SignInController',["$scope",'AuthService','$state','SessionService','$timeout',SignInController]);
 
-  function SignInController($scope,AuthService,$state,SessionService){
+  function SignInController($scope,AuthService,$state,SessionService,$timeout){
     var self = this;
 
     SessionService.ClearCredentials();
@@ -13,12 +13,16 @@
 
       AuthService.signIn(self.userName,self.userPassword)
         .success(function (data) {
-          SessionService.setCredentials(self.userName, self.userPassword ,data);
+          SessionService.setCredentials(data);
           $state.go('apps.index');
         })
         .error(function (data) {
           console.log(data.error_description);
           self.error = data.error_description;
+          $timeout(function() {
+            self.error = undefined;
+          }, 3000);
+
         });
     }
   }
