c0fed790bc32b3fa4eb1998c7f3b70a34de0f42a
Boris Dinkevich <boris@dinkevich.com>
2014-11-19 17:02:08 +0200

    Clean up file

--- client/views/apps/index.js
+++ client/views/apps/index.js
@@ -1,48 +1,39 @@
-
 (function  () {
     'use strict';
-  angular.module('app.apps')
-    .controller('AppsIndexController',['AppsService','appsList','$state','NotificationService',AppsIndexController]);
 
-  function AppsIndexController(AppsService,appsList,$state,NotificationService){
+  angular.module('app.apps')
+    .controller('AppsIndexController',['AppsService', 'appsList', '$state', 'NotificationService', AppsIndexController]);
+
+  function AppsIndexController(AppsService, appsList, $state, NotificationService) {
     var self = this;
 
-    console.log('all apps:');
-    console.log(appsList.data.data);
-
-
-    this.addApp = function(){
+    this.addApp = function() {
       AppsService.add(self.appName,self.appTitle)
         .success(function(data, status, headers, config) {
-          NotificationService.add('success','app was added successfully');
-          $state.go('apps.show',{ name: self.appName });
-      })
+          NotificationService.add('success', 'App was added successfully');
+          $state.go('apps.show', { name: self.appName });
+        })
         .error(function(data, status, headers, config) {
-          NotificationService.add('error',data);
+          NotificationService.add('error', data);
         })
     };
 
-
-
     this.apps = appsList.data.data;
 
-    this.getRibboninfo = function(app){
-      debugger;
+    this.getRibboninfo = function(app) {
       return convertStateNumber(app.DatabaseStatus);
     };
 
-    function convertStateNumber(stateNumber){
-      switch(stateNumber){
+    function convertStateNumber(stateNumber) {
+      switch(stateNumber) {
         case "0":
-          return {class:'ui-ribbon-danger', text:'not'};
         case "1":
-          return {class:"ui-ribbon-warning", text:'process'};
+          return { class: "ui-ribbon-warning", text: 'pending'};
         case "2":
-          return {class:'ui-ribbon-success', text:'connected'};
-
+          return { class: 'ui-ribbon-success', text: 'connected'};
+        default:
+          return { class: 'ui-ribbon-danger', text: 'error'};
       }
     }
-
-
   }
 }());
