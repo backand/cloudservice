b02c9fc6a2beff70cf056f15f6c5321d16d9161a
Boris Dinkevich <boris@dinkevich.com>
2014-11-19 18:05:34 +0200

    Beautify

--- client/common/services/session.service.js
+++ client/common/services/session.service.js
@@ -1,12 +1,12 @@
 (function() {
   'use strict';
 
-  function SessionService($rootScope,$cookieStore) {
-    var self =this;
+  function SessionService($rootScope, $cookieStore) {
+    var self = this;
 
     this.currentUser = $cookieStore.get('globals') ? $cookieStore.get('globals').currentUser : undefined;
 
-    this.getAuthHeader = function(){
+    this.getAuthHeader = function() {
       if (!self.currentUser) {
         return false
       }
@@ -14,27 +14,23 @@
     };
 
     this.setCredentials = function (serverData) {
-
       var user = {
-                   currentUser: {
-                                  access_token : serverData.access_token
-                                }
-                  };
+        currentUser: {
+          access_token : serverData.access_token
+        }
+      };
 
       self.currentUser = user.currentUser;
-      console.log(self.currentUser.access_token);
+      
       $cookieStore.put('globals', user);
     };
 
     this.ClearCredentials = function () {
       $cookieStore.remove('globals');
       self.currentUser = undefined;
-
     };
-
   }
 
   angular.module('common.services', ['ngCookies'])
-    .service('SessionService',['$rootScope','$cookieStore', SessionService])
-
+    .service('SessionService', ['$rootScope', '$cookieStore', SessionService])
 })();
