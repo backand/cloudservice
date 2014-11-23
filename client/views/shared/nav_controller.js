4e58d65ae2af522a1070e62395c1e35f48872e85
nadav <nadav22@gmail.com>
2014-11-23 19:03:04 +0200

    fix ui

--- client/views/shared/nav_controller.js
+++ client/views/shared/nav_controller.js
@@ -2,12 +2,18 @@
 (function  () {
     'use strict';
   angular.module('controllers')
-    .controller('NavCtrl',['$rootScope','$scope', 'filterFilter',NavCtrl]);
+    .controller('NavCtrl',['$rootScope','$scope','$state',NavCtrl]);
 
-  function NavCtrl($rootScope,$scope, filterFilter){
+  function NavCtrl($rootScope,$scope,$state){
     var self = this;
 
 
+    this.appName = $state.params.name;
+
+    $scope.$on('$stateChangeSuccess', function(){
+      self.state = $state.current.name;
+      self.appName = $state.params.name;
+    })
 
   }
 }());
