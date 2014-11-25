5272ffde673f45f4b0b4bd50f5858d759bab71d2
Boris Dinkevich <boris@dinkevich.com>
2014-11-25 17:00:20 +0200

    Added status point in navbar for DB

--- client/views/shared/nav_controller.js
+++ client/views/shared/nav_controller.js
@@ -2,18 +2,46 @@
 (function  () {
     'use strict';
   angular.module('controllers')
-    .controller('NavCtrl',['$rootScope','$scope','$state',NavCtrl]);
+    .controller('NavCtrl', ['$rootScope', '$scope', '$state', 'AppsService', NavCtrl]);
 
-  function NavCtrl($rootScope,$scope,$state){
+  function NavCtrl($rootScope, $scope, $state, AppsService){
     var self = this;
 
-
     this.appName = $state.params.name;
+    this.app = null;
+
+    function loadApp() {
+      if (typeof self.appName !== 'undefined') {
+        AppsService.getCurrentApp(self.appName)
+          .then(function(data) {
+            self.app = data;
+          })
+      }
+    }
 
     $scope.$on('$stateChangeSuccess', function(){
       self.state = $state.current.name;
       self.appName = $state.params.name;
+      loadApp();
     })
+
+
+    this.getDBStatus = function() {
+      if (self.app === null) {
+        return 'unknown';
+      }
+
+      switch(parseInt(self.app.DatabaseStatus)) {
+        case 0:
+        case 2:
+          return 'warning';
+        case 1:
+          return 'success';
+        default:
+          return 'error';
+
+      }
+    }
 
   }
 }());
