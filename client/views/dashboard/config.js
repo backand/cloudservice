1cdf0be7ecb4aaba355d307b1942bd6206d774a1
Boris Dinkevich <boris@dinkevich.com>
2014-11-27 14:55:12 +0200

    Wrap config.js with anon function

--- client/views/dashboard/config.js
+++ client/views/dashboard/config.js
@@ -1,14 +1,16 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('app.dashboard', [])
-  .config(config);
+  angular.module('app.dashboard', [])
+    .config(config);
 
-function config($stateProvider) {
-  $stateProvider
-    .state('dashboard.edit', {
-      parent: 'dashboard',
-      url: '/:name',
-      controller: 'Dashboard as dash',
-      templateUrl: 'views/dashboard/dashboard.html'
-    })
-}
+  function config($stateProvider) {
+    $stateProvider
+      .state('dashboard.edit', {
+        parent: 'dashboard',
+        url: '/:name',
+        controller: 'Dashboard as dash',
+        templateUrl: 'views/dashboard/dashboard.html'
+      })
+  }
+})();
