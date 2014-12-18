17b123f64fd0a09054cfb584b1345e16526f4c3a
Boris Dinkevich <boris@dinkevich.com>
2014-12-18 17:40:24 +0200

    Add tables show

--- client/views/tables/show.js
+++ client/views/tables/show.js
@@ -1,15 +1,23 @@
 (function  () {
   'use strict';
   angular.module('app')
-    .controller('TablesShow', ['$scope', '$state', 'AppsService', 'usSpinnerService', 'NotificationService', 'DatabaseService', TablesShow]);
+    .controller('TablesShow', ['$scope', '$state', 'AppsService', 'usSpinnerService', 'NotificationService', 'TablesService', TablesShow]);
 
-  function TablesShow($scope, $state, AppsService, usSpinnerService, NotificationService, DatabaseService, DatabaseNamesService) {
+  function TablesShow($scope, $state, AppsService, usSpinnerService, NotificationService, TablesService) {
     var self = this;
     var currentApp;
 
     AppsService.getCurrentApp($state.params.name)
       .then(function(data) {
         currentApp = data;
+
+        TablesService.get()
+          .then(function (data) {
+            self.tables = data.data;
+            console.log("Tables");
+            console.log(self.tables);
+          });
+
       }, function(err) {
         NotificationService('error', 'Can not get current app info');
       });
