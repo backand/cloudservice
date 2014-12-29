0ed359f7793d9c9cf59b48d5bc60bf2463f483c1
Yoram Kornatzky <kornatzky@gmail.com>
2014-12-29 10:16:32 +0200

    Columns List
    Column details

--- client/views/shared/nav_controller.js
+++ client/views/shared/nav_controller.js
@@ -101,47 +101,10 @@
     });
 
     $scope.tables = [];
-    // [
-    //   { name: "A1" },
-    //   { name: "A2" },
-    //   { name: "A3" },
-    //   { name: "A4" },
-    //   { name: "A5" },
-    //   { name: "A6" },
-    //   { name: "A7" },
-    //   { name: "A1" },
-    //   { name: "A2" },
-    //   { name: "A3" },
-    //   { name: "A4" },
-    //   { name: "A5" },
-    //   { name: "A6" },
-    //   { name: "A7" },
-    //   { name: "A1" },
-    //   { name: "A2" },
-    //   { name: "A3" },
-    //   { name: "A4" },
-    //   { name: "A5" },
-    //   { name: "A6" },
-    //   { name: "A7" },
-    //   { name: "A1" },
-    //   { name: "A2" },
-    //   { name: "A3" },
-    //   { name: "A4" },
-    //   { name: "A5" },
-    //   { name: "A6" },
-    //   { name: "A7" },
-    //   { name: "A1" },
-    //   { name: "A2" },
-    //   { name: "A3" },
-    //   { name: "A4" },
-    //   { name: "A5" },
-    //   { name: "A6" },
-    //   { name: "A7" }
-    // ];
-
+  
     $scope.fetchTables = function(){
-      $log.debug("appName", $state.params.name);
-      AppsService.getCurrentApp($state.params.name).then(
+      $log.debug("appName", self.appName);
+      AppsService.getCurrentApp(self.appName).then(
         function(data) {
           $log.debug(data);
           self.currentApp = data;
--- client/views/tables/show_single_table.js
+++ client/views/tables/show_single_table.js
@@ -12,76 +12,28 @@
     var currentApp;
     self.messages = ["no stats yet..."];
     self.alertClass = "";
+    $scope.fields = [];
+    
 
-    ColumnsService.get($state.params.appName, $state.params.tableName)
+    $scope.switchTab = function(tab) {
+      $log.debug("switchTab", tab);
+      ColumnsService.get($state.params.appName, $state.params.tableName)
       .then(function(data) {
         $log.debug("columns success:", data);
-        // data.data.fields
+        
+        $scope.fields = data.data.fields;
       }, 
       function(err) {
         $log.debug("columns failure:", err);
         NotificationService.add('error', 'Can not get table info');
       }
     );
+    };
 
-    this.reloadTables = function() {
-      usSpinnerService.spin("loading");
-
-      TablesService.get(self.currentApp.Name)
-        .then(function (data) {
-          usSpinnerService.stop("loading");
-          self.tables = data.data.data;
-        }, function(err) {
-          usSpinnerService.stop("loading");
-          NotificationService.add('error', 'Can not get tables list');
-      });
-    }
-
-    this.sync = function() {
-      self.syncing = true;
-      NotificationService.add('info', 'Sync takes 1-2 minutes');
-      TablesService.sync(self.currentApp.Name)
-        .then(function (data) {
-          self.syncing = false;
-          //update messages
-          self.messages = [];
-          self.messages.push('New tables added: ' + data.data.added);
-          self.messages.push('Tables with error:' + (data.data.newTables - data.data.added));
-
-          if(data.data.errors !== '')
-          {
-
-              //data.data.errors = data.data.errors.replace(/\r?\n/g, "%0D%0A");
-              NotificationService.add('error', 'Synchronizing tables completed with errors: ' + data.data.errors);
-              self.alertClass = 'tables-alert-with-errors';
-              self.messages.push('Errors: ' + data.data.errors);
-          }
-          else
-          {
-              NotificationService.add('success', 'Synchronizing tables was completed successfully');
-              self.alertClass = '';
-          }
-          //refresh tables list
-          self.reloadTables();
-
-        }, function(err) {
-          self.syncing = false;
-          NotificationService.add('error', 'Can not sync tables');
-        });
-    }
-
-    this.getMessages = function() {
-        return self.messages;
-    }
-
-    //Sync the tables when loading the page
-    // angular.element(document).ready(function () {
-    //     if($state.params.sync === '1')
-    //         $timeout(function() {
-    //             $state.params.sync = 0;
-    //             angular.element('#sync_button')[0].click();
-    //         }, 100);
-    // });
+    $scope.selectedField = null;
+    $scope.showField = function(field) {
+      $scope.selectedField = field;
+    };
 
   }
 }());
