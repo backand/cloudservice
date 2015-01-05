61c542c7381e375b8793f54f4416ac85240fc217
yariv@backand.com <yariv@backand.com>
2015-01-05 14:56:08 +0200

    Fields settings

--- client/views/tables/show_single_table.js
+++ client/views/tables/show_single_table.js
@@ -10,25 +10,43 @@
     self.tableName = $state.params.tableName;
     self.messages = ["no stats yet..."];
     self.alertClass = "";
+    $scope.excluded=true;
     $scope.fields = [];
     $scope.fieldTypesRange = ["String", "DateTime", "Integer"];
 
 
-    $scope.switchTab = function(tab) {
+    $scope.switchTab = function (tab) {
       ColumnsService.get($state.params.name, $state.params.tableName)
-      .then(function(data) {
-        $scope.fields = data.data.fields;
-      },
-      function(err) {
-        NotificationService.add('error', 'Can not get table info');
-      }
-    );
+        .then(function (data) {
+          $scope.fields = data.data.fields;
+        },
+        function (err) {
+          NotificationService.add('error', 'Can not get table info');
+        }
+      );
     };
 
     $scope.selectedField = null;
-    $scope.showField = function(field) {
+    $scope.showField = function (field) {
       $scope.selectedField = field;
     };
+    self.sumbitForm = function () {
+      self.loading = true;
+      try {
+        ColumnsService.update(self.appName, self.appTitle)
+          .success(function (data) {
+            NotificationService.add('success', 'Application settings updated successfully');
+            self.loading = false;
+          })
+          .error(function (err) {
+            NotificationService.add('Error', 'waiting for Relly Rivlin');
+            self.loading = false;
+          })
 
+      }
+      catch (ex) {
+        NotificationService.add('Error', 'waiting for Relly Rivlin');
+      }
+    }
   }
 }());
