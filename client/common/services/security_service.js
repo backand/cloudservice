25b31a26e1fe66eacd9da22feaf8d935658a8eaa
yariv@backand.com <yariv@backand.com>
2015-01-07 22:42:19 +0200

    merg commit

--- client/common/services/security_service.js
+++ client/common/services/security_service.js
@@ -3,16 +3,35 @@
 
   function SecurityService($http, $q, CONSTS) {
 
-    this.getUsers = function(appName, pageSize){
+    this.getData = function(appName, pageSize,tableName){
       return $http({
         method: 'GET',
-        url: CONSTS.appUrl + '/1/table/data/v_durados_User?pageSize='+String(pageSize),
+        url: CONSTS.appUrl + '/1/table/data/'+tableName+'?pageSize='+String(pageSize),
         headers: {
           'AppName': appName
         },
         params: {
           'sort' : '[{fieldName:"id", order:"desc"}]'
         }
+      });
+    };
+
+    this.getUsers = function(appName, pageSize){
+      return this.getData(appName, pageSize,'v_durados_User')
+
+    };
+    this.getRoles = function(appName, pageSize){
+      return this.getData(appName, pageSize,'durados_UserRole')
+
+    };
+    this.update = function(appName, tableName ,rowData){
+      return $http({
+        method: 'PUT',
+        url: CONSTS.appUrl + '/1/table/data/'+tableName+'/'+rowData.ID,
+        headers: {
+          'AppName': appName
+        },
+        data:rowData
       });
     };
 
--- client/views/security/users.js
+++ client/views/security/users.js
@@ -6,49 +6,55 @@
   function SecurityUsers($stateParams, $log, NotificationService, SecurityService, $scope) {
 
     var self = this;
-    self.myData = [];
-    self.gridOptions= {
-      data:{},
-      columnDefs: [
+    self.tableName='v_durados_user';
+    self.roles = [];
+    self.gridOptions= {};
+    self.gridOptions.columnDefs= [
         { name: 'FirstName' },
         { name: 'LastName' },
-        { name: 'Username' },
+        { name: 'Username',enableCellEdit: false },
         { name: 'Email' },
-        { name: 'durados_User_Role',displayName:'Role' },
-        { name: 'IsApproved' }
-      ]
-    };
-    var columnDefs = [{name:'field1'}, {name:'field2'}];
+        { name: 'durados_User_Role',displayName:'Role' ,editDropdownOptionsArray:self.roles},
+        { name: 'IsApproved',displayName:'Approved',type: 'boolean' }
+      ];
+    //self.gridOptions.rowEditWaitInterval =750;
+self.addData = function(){
+
+  $scope.gridOpts.data.push({
+    "FirstName": "New " + n,
+    "LastName": "Person " + n,
+    "Username": "abc",
+    "Email": true,
+    "durados_User_Role" :"",
+    IsApproved:false
+  });}
     SecurityService.getUsers($stateParams.name, 20)
       .then(usersSuccsessHandler, errorHandler);
 
     function usersSuccsessHandler(data) {
-      self.gridOptions.data = data.data.data;
+
+      self.gridOptions.data =data.data.data;
+      SecurityService.getRoles($stateParams.name, 20)
+        .then(rolesSuccsessHandler, errorHandler);
+
     }
+    function rolesSuccsessHandler(data){
+      self.roles= data.data.data;
+    }
+    $scope.saveRow = function( rowEntity ) {
+      // create a fake promise - normally you'd use the promise returned by $http or $resource
+      var promise = SecurityService.update($stateParams.name,self.tableName,rowEntity);
+      $scope.gridApi.rowEdit.setSavePromise( $scope.gridApi.grid, rowEntity, promise.promise );
 
-    //this.myData = [
-    //  {
-    //    "firstName": "Cox",
-    //    "lastName": "Carney",
-    //    "company": "Enormo",
-    //    "employed": true
-    //  },
-    //  {
-    //    "firstName": "Lorraine",
-    //    "lastName": "Wise",
-    //    "company": "Comveyer",
-    //    "employed": false
-    //  },
-    //  {
-    //    "firstName": "Nancy",
-    //    "lastName": "Waters",
-    //    "company": "Fuelton",
-    //    "employed": false
-    //  }
-    //];
-
+    };
+    self.gridOptions.onRegisterApi = function(gridApi){
+      //set gridApi on scope
+      $scope.gridApi = gridApi;
+      $scope.gridApi.rowEdit.on.saveRow($scope, $scope.saveRow);
+    };
     function errorHandler(error, message) {
       NotificationService.add('error', message);
+      self.gridOptions
       $log.debug(error);
     }
 
@@ -60,6 +66,7 @@
       '$log',
       'NotificationService',
       'SecurityService',
+      '$scope',
       SecurityUsers
     ]);
 
