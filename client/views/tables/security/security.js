4d39dd2dac306919872d1eba48c40ad585977f59
nirkaufman <Nir@500tech.com>
2015-01-14 18:22:36 +0200

    security matrix

--- client/views/tables/security/security.js
+++ client/views/tables/security/security.js
@@ -2,8 +2,32 @@
 
   function SecurityController() {
 
+    var self = this;
+
+    self.sTemplate = [
+      {
+        title: 'Admin',
+        permissions: {
+          read: true,
+          write: false,
+          edit: true,
+          delete: false
+        }
+      },
+
+      {
+        title: 'User',
+        permissions: {
+          read: true,
+          write: false,
+          edit: false,
+          delete: false
+        }
+      }
+    ]
+
   }
 
   angular.module('app')
-    .controller('SecurityController',SecurityController);
+    .controller('SecurityController', SecurityController);
 }());
