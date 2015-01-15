e2abc66605bdd4c1c810fd6e35c43c1b28a79422
nirkaufman <Nir@500tech.com>
2015-01-15 11:59:07 +0200

    security matrix

--- client/common/services/apps_service.js
+++ client/common/services/apps_service.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   function appsService($http, $q, CONSTS, DatabaseNamesService) {
@@ -17,27 +17,27 @@
     apps.deferred = $q.defer();
 
     function updateAppNames() {
-        apps.names = [];
-        apps.list.forEach(function(item) {
+      apps.names = [];
+      apps.list.forEach(function (item) {
         apps.names.push(item.Name);
         apps.status[item.Name] = item.DatabaseStatus;
       })
     }
 
-    this.setCurrentApp = function(data){
+    this.setCurrentApp = function (data) {
       currentApp = data;
       currentApp.databaseName = data.Database_Source ? DatabaseNamesService.getDBSource(data.Database_Connection.Database_Source) : undefined;
     };
 
-    this.getCurrentApp = function(appName){
+    this.getCurrentApp = function (appName) {
       var deferred = $q.defer();
       self.find(appName)
-        .success(function (data){
+        .success(function (data) {
           self.setCurrentApp(data);
-          currentApp.myStatus = {status : data.DatabaseStatus, oldStatus: apps.status[appName]};
+          currentApp.myStatus = {status: data.DatabaseStatus, oldStatus: apps.status[appName]};
           deferred.resolve(currentApp);
         })
-        .error(function(err){
+        .error(function (err) {
           deferred.reject(err);
         });
 
@@ -45,13 +45,13 @@
 
     };
 
-    this.getAlert  = function(appName) {
+    this.getAlert = function (appName) {
       if (apps.alerts[appName] == null) {
         this.appDbStat(appName)
           .success(function (data) {
             if (data.tableCount == 0)
               apps.alerts[appName] = "Your database has no tables! go to <a href=''>Database Create</a> and run our existing schemas or use any Admin tool to add tables";
-              return apps.alerts[appName];
+            return apps.alerts[appName];
           })
       }
 
@@ -59,79 +59,77 @@
 
     };
 
-    this.setAlert  = function(appName, msg) {
-          apps.alerts[appName] = msg;
+    this.setAlert = function (appName, msg) {
+      apps.alerts[appName] = msg;
     };
 
-    function searchStringInArray (str, strArray) {
-      for (var j=0; j<strArray.length; j++) {
+    function searchStringInArray(str, strArray) {
+      for (var j = 0; j < strArray.length; j++) {
         if (strArray[j].match(str)) return j;
       }
       return -1;
     }
 
-    this.appNames = function(appName) {
-      if (searchStringInArray(appName, apps.names) === -1){
+    this.appNames = function (appName) {
+      if (searchStringInArray(appName, apps.names) === -1) {
         apps.names.push(appName);
       }
       return apps.names;
     };
 
-    this.all = function() {
+    this.all = function () {
 
       return $http({
         method: 'GET',
         url: CONSTS.appUrl + '/admin/myApps?pageSize=50'
       })
-      .success(function (data) {
-      if (data) {
-          apps.list = data.data;
-          updateAppNames();
-          apps.deferred.resolve(apps);
-      }
-      })
-      .error(function (error) {
-        apps.deferred.reject(error);
-      });
+        .success(function (data) {
+            apps.list = data.data;
+            updateAppNames();
+        })
+        .error(function (error) {
+          //apps.deferred.reject(error);
+        });
 
 
     };
 
-    this.refresh = function() {
-     return  $http({
+    this.refresh = function () {
+      return $http({
         method: 'GET',
         url: CONSTS.appUrl + '/admin/myApps?pageSize=50'
       });
 
     };
-    this.find = function(appName) {
+    this.find = function (appName) {
       return $http({
         method: 'GET',
-        url: CONSTS.appUrl + '/admin/myApps/'+appName+'?deep=true'});
+        url: CONSTS.appUrl + '/admin/myApps/' + appName + '?deep=true'
+      });
     };
 
-    this.add = function(name, title) {
+    this.add = function (name, title) {
       var deferred = $q.defer();
 
       $http({
         method: 'POST',
         url: CONSTS.appUrl + '/admin/myApps/',
         data: {
-                Name: name,
-                Title: title
-              }
+          Name: name,
+          Title: title
+        }
       })
-      .success(function(data) {
-        deferred.resolve(data.data);
-      })
-      .error(function(error) {
-        deferred.reject(error);
-      });
+        .success(function (data) {
+          deferred.resolve(data.data);
+        })
+        .error(function (error) {
+          deferred.reject(error);
+        });
 
       return deferred.promise;
     };
 
-    this.appDbStat = function(appName) {
+    this.appDbStat = function (appName) {
       return $http({
         method: 'GET',
         url: CONSTS.appUrl + '/1/app/dbStat',
@@ -139,24 +137,24 @@
       });
     };
 
-    this.update = function(name,data) {
+    this.update = function (name, data) {
       return $http({
         method: 'PUT',
-        url: CONSTS.appUrl + '/admin/myApps/'+name,
+        url: CONSTS.appUrl + '/admin/myApps/' + name,
         data: data
 
       });
     };
 
-    this.delete = function(name) {
+    this.delete = function (name) {
       return $http({
         method: 'DELETE',
-        url: CONSTS.appUrl + '/admin/myApps/'+name
+        url: CONSTS.appUrl + '/admin/myApps/' + name
       });
     };
   }
 
   angular.module('common.services')
-    .service('AppsService',['$http', '$q', 'CONSTS','DatabaseNamesService', appsService]);
+    .service('AppsService', ['$http', '$q', 'CONSTS', 'DatabaseNamesService', appsService]);
 
 })();
