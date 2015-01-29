041f51eac5437cea13a353f3cf25dc5523b7878d
yariv@backand.com <yariv@backand.com>
2015-01-29 22:51:24 +0200

    fix new rule modal validation

--- client/views/security/auth/auth.js
+++ client/views/security/auth/auth.js
@@ -3,34 +3,38 @@
  */
 (function () {
 
-  function SecurityAuth( $stateParams, NotificationService, SecurityService,AppsService, $scope) {
+  function SecurityAuth($stateParams, NotificationService, SecurityService, AppsService, $scope) {
 
     var self = this;
     (function init() {
       self.appName = SecurityService.appName = AppsService.appName = $stateParams.name;
-      self.data={settings:{},privateApp:false};
+      self.data = {settings: {}, allowAnonymous: false, privateApp: false};
       self.updateAppAuth = updateAppAuth;
-     //secureLevel= "RegisteredUsers";
+      //secureLevel= "RegisteredUsers";
       loadConfigurationData();
     }());
 
-    $scope.$watch('auth',updateAppAuth,true);
-    function updateAppAuth(newVal,oldVal,scope)
-    {
-      if(oldVal.data.settings.__metadata) {
-       if(self.data.privateApp)
-         self.data.settings.secureLevel ="RegisteredUsers";
-        else
-         self.data.settings.secureLevel ="AllUsers";
+    $scope.$watch('auth', updateAppAuth, true);
+    function updateAppAuth(newVal, oldVal, scope) {
 
-        AppsService.update( self.appName, newVal.data);
-      }
+      if (!oldVal.data.settings.__metadata)
+        return;
+      var data = angular.copy(newVal);
+      if (newVal.data.allowAnonymous)
+        data.data.settings.secureLevel = "AllUsers";
+      else
+        data.data.settings.secureLevel = "RegisteredUsers";
+      if (newVal.data.privateApp != oldVal.data.privateApp)
+        data.data.settings.enableUserRegistration = !newVal.data.privateApp;
+      AppsService.update(self.appName, data.data);
+
     }
+
     function loadConfigurationData() {
-      AppsService.getCurrentApp(self.appName).then(setDbInfo,errorHandler);
+      AppsService.getCurrentApp(self.appName).then(setDbInfo, errorHandler);
 
       SecurityService.getRoles().then(rolesSuccessHandler, errorHandler);
- }
+    }
 
     function rolesSuccessHandler(data) {
       self.roles = data.data.data;
@@ -38,19 +42,19 @@
 
     function errorHandler(error, message) {
       NotificationService.add('error', message);
-     }
+    }
 
-    function setDbInfo(data){
-      self.data.settings =data.settings;
+    function setDbInfo(data) {
+      self.data.settings = data.settings;
 
-      self.data.privateApp =self.data.settings.secureLevel =="RegisteredUsers";
-
+      self.data.allowAnonymous = self.data.settings.secureLevel == "AllUsers";
+      self.data.privateApp = !self.data.settings.enableUserRegistration;
     }
   }
 
   angular.module('app')
     .controller('SecurityAuth', [
-     '$stateParams',
+      '$stateParams',
       'NotificationService',
       'SecurityService',
       'AppsService',
