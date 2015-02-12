33430fdb892c1bbda8a18a95fdb3f3fb06486019
Shmuela <shmuela@500tech.com>
2015-02-12 17:31:48 +0200

    Select parameters from list in modal

--- client/views/db_queries/query.js
+++ client/views/db_queries/query.js
@@ -2,15 +2,16 @@
 (function  () {
   'use strict';
   angular.module('app.dbQueries')
-    .controller('DbQueryController', ['$state', '$stateParams', 'DbQueriesService', 'ConfirmationPopup', 'NotificationService', DbQueryController]);
+    .controller('DbQueryController', ['$scope', '$state', '$stateParams', 'DbQueriesService', 'ConfirmationPopup', 'NotificationService', 'DictionaryService', DbQueryController]);
 
-  function DbQueryController($state, $stateParams, DbQueriesService, ConfirmationPopup, NotificationService) {
+  function DbQueryController($scope, $state, $stateParams, DbQueriesService, ConfirmationPopup, NotificationService, DictionaryService) {
     var self = this;
     init();
 
 
     function init() {
       DbQueriesService.getQueries($stateParams.name).then(function() {
+        self.openParamsModal = false;
         self.new = (!$stateParams.queryId);
         self.editMode = self.new;
         if (self.new) {
@@ -44,20 +45,14 @@
       self.query.allowSelectRoles = allowSelectRolesArray.join(',');
     }
 
-    self.saveOrEdit = function () {
-      if (self.editMode) {
-        saveQuery();
-      }
-      else {
-        editQuery();
-      }
-      self.editMode = !self.editMode;
-    };
-
-    function saveQuery() {
+    this.saveQuery = function () {
+      //TODO: spinner start
+      self.openParamsModal = false;
       rolesToString();
       DbQueriesService.saveQuery(self.query)
         .then(function (query) {
+          self.editMode = false;
+          //TODO: spinner stop
           var params = {
             name: $stateParams.name,
             queryId: query.__metadata.id
@@ -65,19 +60,21 @@
           $state.go('dbQueries.query', params);
         });
 
-    }
+    };
 
-    function editQuery() {
-    }
+    this.editQuery = function () {
+      self.editMode = true;
+      //populateDictionaryItems();
+    };
 
-    self.cancel = function () {
+    this.cancel = function () {
       ConfirmationPopup.confirm('Changes will be lost. Are sure you want to cancel editing?')
         .then(function(result){
           result ? init() : false;
         });
     };
 
-    self.deleteQuery = function () {
+    this.deleteQuery = function () {
       ConfirmationPopup.confirm('Are you sure you want to delete the query?')
         .then(function (result) {
           if (!result)
@@ -91,6 +88,51 @@
           $state.go('apps.show', {name: $stateParams.name});
         })
     };
+
+
+    function populateDictionaryItems() {
+      DictionaryService.get().then(function (data) {
+        var raw = data.data;
+        var keys = Object.keys(raw);
+        this.dictionaryItems = {
+          headings: {
+            tokens: keys[0],
+            props: keys[1],
+            parameters: 'Parameters'
+          },
+          data: {
+            tokens: raw[keys[0]],
+            props: raw[keys[1]],
+            parameters: []
+          }
+        };
+      });
+    }
+
+    this.toggleParametersModal = function () {
+      self.openParamsModal = !self.openParamsModal;
+    };
+
+    this.insertParamAtChar = function (elementId, param) {
+      $scope.$parent.$broadcast('insert:placeAtCaret', [elementId, param]);
+    };
+
+    var paramRegex = /{{([^}]*)}}/g; //anything between '{{' and '}}' except '}'
+    this.getParameters = function () {
+      if (self.query) {
+        var paramsMatch = self.query.parameters.replace(/ /g, '').split(',');
+        var queryMatch = [];
+        var queryMatchWithBrackets = self.query.sQL.match(paramRegex);
+        if (queryMatchWithBrackets != null)
+          queryMatchWithBrackets.forEach(function (match) {
+            queryMatch.push(match.substring(2, match.length-2).replace(/ /g, ''));
+          });
+
+        var parameters = _.union(paramsMatch, queryMatch);
+        self.query.parameters = parameters.join(',');
+        return parameters;
+      }
+    }
 
   }
 }());
