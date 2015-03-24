2c8c56d3105293724b6ae754c585dd5673ec2c4b
Shmuela <shmuela@500tech.com>
2015-03-24 20:23:48 +0200

    Connect to DB page - added templates and custom schema

--- client/views/database/edit.js
+++ client/views/database/edit.js
@@ -1,9 +1,9 @@
 (function  () {
   'use strict';
   angular.module('app.apps')
-    .controller('DatabaseEdit', ['$scope', 'AppsService', '$stateParams', '$state', 'DatabaseNamesService', 'NotificationService', 'DatabaseService','usSpinnerService','ConfirmationPopup','$analytics', DatabaseEdit]);
+    .controller('DatabaseEdit', ['$http', 'AppsService', '$stateParams', '$state', 'DatabaseNamesService', 'NotificationService', 'DatabaseService', 'usSpinnerService', 'ConfirmationPopup', '$analytics', DatabaseEdit]);
 
-  function DatabaseEdit($scope, AppsService, $stateParams, $state, DatabaseNamesService, NotificationService, DatabaseService,usSpinnerService,ConfirmationPopup,$analytics) {
+  function DatabaseEdit($http, AppsService, $stateParams, $state, DatabaseNamesService, NotificationService, DatabaseService, usSpinnerService, ConfirmationPopup, $analytics) {
 
     var self = this;
 
@@ -58,7 +58,7 @@
             self.data.sshPort   = dataIn.SshPort;
             self.data.sshPassword   = dataIn.SshPassword;
             self.data.sshPrivateKey   = dataIn.SshPrivateKey;
-          })
+          });
         usSpinnerService.stop("loading");
     }
 
@@ -72,8 +72,8 @@
 
         DatabaseService.createDB($state.params.name, product, sampleApp)
         .success(function(data){
-            $analytics.eventTrack('addedDbTables',{product:product,sampleApp:sampleApp,includeData:self.includeData});
-            NotificationService.add('info','Creating new database... It may take 1-2 minutes');
+            $analytics.eventTrack('addedDbTables', {product: product, sampleApp: sampleApp, includeData: self.includeData});
+            NotificationService.add('info', 'Creating new database... It may take 1-2 minutes');
           $state.go('getting-started-open', {isnew: 'new'});
         })
         .error(function(err){
@@ -101,7 +101,7 @@
       else {
           DatabaseService.connect2DB($state.params.name, self.data)
               .success(function (data) {
-              $analytics.eventTrack('connectExisting',{product:self.data.product});
+              $analytics.eventTrack('connectExisting', {product: self.data.product});
 
               NotificationService.add('info', 'Connecting to the database...');
                   $state.go('apps.index', {name: $state.params.name})
@@ -114,12 +114,67 @@
     };
 
     self.back = function(){
-      $state.go('apps.show',({name:$state.params.name}));
+      $state.go('apps.show', {name: $state.params.name});
     };
 
     self.Confirmation = function(msg){
-      ConfirmationPopup.confirm(msg,'Ok','',true,false);
-    }
+      ConfirmationPopup.confirm(msg, 'Ok', '', true, false);
+    };
+
+
+    // Ace Templates
+
+    self.ace = {
+      onLoad: function(_editor) {
+        self.ace.editor = _editor;
+      }
+    };
+
+    self.defaultTemplate = {title: "Create your own", template: 'create_your_own'};
+
+    self.templates = [
+      {title: "Game Shop", template: 'game_shop'},
+      {title: "E-commerce Campaign", template: 'ecommerce_campaign'},
+      {title: "Advertising System", template: 'advertising_system'}
+    ];
+
+    self.getFile = function (file) {
+      return $http({
+        method: 'GET',
+        url: '/views/database/db_templates/' + file.template + '.json'
+      }).then (function (result) {
+        return angular.toJson(result.data, true);
+      })
+    };
+
+    self.showFile = function (file) {
+      self.getFile(file)
+        .then(function (result) {
+          self.template = result;
+          self.activeFile = file;
+        })
+    };
+
+    self.showFile(self.templates[0]);
+
+    self.getFile(self.defaultTemplate)
+      .then(function (result) {
+        self.customSchema = result;
+      });
+
+    self.customize = function () {
+      self.customMode = true;
+    };
+
+    // Field Types
+
+    self.fieldTypes = ['ShortText', 'LongText', 'DateTime', 'Numeric', 'Boolean', 'SingleSelect', 'MultiSelect'];
+
+    self.insertTypeAtChar = function (param) {
+      setTimeout(function() { // DO NOT USE $timeout - all changes to ui-ace must be done outside digest loop, see onChange method in ui-ace
+        self.ace.editor.insert('"' + param + '"');
+      });
+    };
 
   }
 }());
