5eba959490eac95ccd281231138a7d8ff4c6dffb
Shmuela <shmuela@500tech.com>
2015-10-07 11:54:49 +0300

    remove 'headers' object from logged http json

--- client/common/services/stringify_http_function.js
+++ client/common/services/stringify_http_function.js
@@ -6,7 +6,7 @@
   function stringifyHttp (CONSTS) {
 
     return function (http) {
-      var stringifiedHttp = 'return $http (' + angular.toJson(http, true) + ');';
+      var stringifiedHttp = 'return $http (' + angular.toJson(_.omit(http, 'headers'), true) + ');';
       stringifiedHttp = stringifiedHttp.replace(/"([\d\w\s]+)"\s*:/g, '$1:');
       stringifiedHttp = stringifiedHttp.replace(/"/g, "'");
       stringifiedHttp = stringifiedHttp.replace("'" + CONSTS.appUrl, "Backand.getApiUrl() + '");
