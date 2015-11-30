faaf96cc681928a81f591c98905566f9c65812ee
Tamir Kfir <tamir.kk@gmail.com>
2015-11-30 23:12:25 +0200

    - add collections fields

--- client/views/tables/model/erd_model/edit_field.js
+++ client/views/tables/model/erd_model/edit_field.js
@@ -9,6 +9,7 @@
       'newModel',
       'DbDataModel',
       'AppsService',
+      'TablesService',
       '$filter',
       EditFieldController
     ]);
@@ -20,6 +21,7 @@
                                newModel,
                                DbDataModel,
                                AppsService,
+                               TablesService,
                                $filter) {
     var self = this;
 
@@ -28,15 +30,25 @@
     self.fieldName = fieldName;
     self.newModel = newModel;
     self.editFieldForm = 'edit-field';
-
+    self.typeOptions = [
+      'string',
+      'text',
+      'datetime',
+      'float',
+      'boolean',
+      'collection'
+    ];
+    self.objectOptions = TablesService.tables;
 
 
     self.addField = function () {
       var newModelObject = JSON.parse(self.newModel.schema);
-      var fieldToAdd = {};
-      fieldToAdd[self.fieldName] = {type: self.fieldType};
-      var object = _.find(newModelObject, {name: self.tableName});
-      _.extend(object.fields, fieldToAdd);
+      if (self.fieldType == 'collection') {
+        createCollectionField(newModelObject, self.fieldName, self.relatedObject.name, self.viaField);
+      }
+      else {
+        createSimpleField(newModelObject, self.fieldName, self.fieldType, self.tableName);
+      }
       //DbDataModel.saveCustomSchema(self.appName, JSON.stringify(newModelObject));
       //modalInstance.dismiss('added');
       modalInstance.close({model: newModelObject});
@@ -45,6 +57,32 @@
     self.cancelEditField = function () {
       modalInstance.dismiss('cancel');
     };
+
+
+    function createCollectionField(model, fieldName, relatedObject, viaField) {
+      // Create field on the selected object
+      var fieldToAdd = {};
+      fieldToAdd[fieldName] = {collection: relatedObject, via: viaField};
+      addGenericField(model, self.tableName, fieldToAdd);
+
+      // Create field on the related object
+      fieldToAdd = {};
+      fieldToAdd[viaField] = {object: self.tableName};
+      addGenericField(model, relatedObject, fieldToAdd);
+    }
+
+    // Create field of a simple type, e.g {type: string}
+    function createSimpleField(model, fieldName, fieldType, tableName) {
+      var fieldToAdd = {};
+      fieldToAdd[fieldName] = {type: fieldType};
+      addGenericField(model, tableName, fieldToAdd);
+    }
+
+    // Add a field from an existing field object given to the function
+    function addGenericField(model, tableName, fieldToAdd) {
+      var object = _.find(model, {name: tableName});
+      _.extend(object.fields, fieldToAdd);
+    }
 
   }
 }());
