9cd848c2454aa8beb399e91c7736bbdb8f8a93bc
Tamir Kfir <tamir.kk@gmail.com>
2016-02-11 23:04:06 +0200

    add refresh cache when setting config, added placeholder for current version and added notification when finish

--- client/views/apps/configuration.js
+++ client/views/apps/configuration.js
@@ -2,41 +2,57 @@
 
   'use strict';
   angular.module('backand.apps')
-    .controller('AppConfiguration', ['AppsService', 'Upload', AppConfiguration]);
+    .controller('AppConfiguration', ['AppsService', 'Upload', 'NotificationService', 'usSpinnerService', AppConfiguration]);
 
-  function AppConfiguration(AppsService, Upload) {
+  function AppConfiguration(AppsService, Upload, NotificationService, usSpinnerService) {
     var self = this;
     self.appName = AppsService.currentApp.Name;
+
     init();
 
     function init() {
+      usSpinnerService.spin('loading');
+      self.currentVersion = '1.0.0';
       AppsService.getBackupVersions().then(function (data) {
         self.latestConfigurations = _.map(data.data.versions, function (value) {
           return {version: value, dateTime: new Date()};
         });
+        usSpinnerService.stop('loading');
       });
     }
 
     self.uploadConfiguration = function (file) {
+      usSpinnerService.spin('loading');
       Upload.dataUrl(file, true).then(function (dataUrl) {
-        AppsService.uploadBackup(file.name, dataUrl.replace('data:;base64,', ''));
+        AppsService.uploadBackup(file.name, dataUrl.replace('data:;base64,', '')).then(function (data) {
+          AppsService.reset(self.appName).then(function (data) {
+            init();
+            NotificationService.add('success', 'Configuration imported successfully')
+          });
+        });
       })
     };
 
     self.setConfiguration = function (version) {
+      usSpinnerService.spin('loading');
       AppsService.restoreBackup(version).then(function (data) {
-        init();
+        AppsService.reset(self.appName).then(function (data) {
+          init();
+          NotificationService.add('success', 'Configuration rolled to version ' + version + ' successfully')
+        });
       });
     };
 
     self.downloadConfiguration = function (version) {
-      AppsService.downloadBackup(version).then(function (data, status, headers) {
+      usSpinnerService.spin('loading');
+      AppsService.downloadBackup(version).then(function (data) {
         var blob = new Blob([data.data], {type: 'application/octet-stream'});
         var downloadUrl = (window.URL || window.webkitURL).createObjectURL(blob);
         var anchor = document.createElement("a");
         anchor.download = self.appName + ".zip";
         anchor.href = downloadUrl;
         anchor.click();
+        usSpinnerService.stop('loading');
       });
     };
   }
