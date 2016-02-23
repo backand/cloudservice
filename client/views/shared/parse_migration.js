91786ac68d17e1bff48925f81cdcaf038cecfd28
Tamir Kfir <tamir.kk@gmail.com>
2016-02-23 23:12:37 +0200

    validation in parse migration

--- client/views/shared/parse_migration.js
+++ client/views/shared/parse_migration.js
@@ -3,9 +3,9 @@
 
   angular.module('controllers')
     .controller('ParseMigrationController',
-      ['AppsService', 'ParseService', 'AnalyticsService', 'DatabaseService', 'ModelService', 'usSpinnerService', '$modalInstance', '$scope', ParseMigrationController]);
+    ['AppsService', 'ParseService', 'AnalyticsService', 'DatabaseService', 'ModelService', 'usSpinnerService', '$modalInstance', '$scope', 'NotificationService', ParseMigrationController]);
 
-  function ParseMigrationController(AppsService, ParseService, AnalyticsService, DatabaseService, ModelService, usSpinnerService, $modalInstance, $scope) {
+  function ParseMigrationController(AppsService, ParseService, AnalyticsService, DatabaseService, ModelService, usSpinnerService, $modalInstance, $scope, NotificationService) {
     var self = this;
     self.parseSchemeDescription = "Paste your Parse schema here:"
     ;
@@ -13,9 +13,9 @@
     self.dataExportDescription = "Paste your Parse data zip file link here:";
 
     self.dataSchemePlaceholder = "Parse scheme goes here:\n" +
-      "{\"results\":[{\"className\":\"_User\", ...\n" +
-      "{\"className\":\"_Product\",\"fields\":{\ ...\n" +
-      "\"targetClass\":\"_User\"}}}]}";
+    "{\"results\":[{\"className\":\"_User\", ...\n" +
+    "{\"className\":\"_Product\",\"fields\":{\ ...\n" +
+    "\"targetClass\":\"_User\"}}}]}";
 
     self.schemaPopoverTemplateUrl = "views/shared/parse_schema_popover.html";
 
@@ -25,13 +25,18 @@
 
     self.create = function () {
       usSpinnerService.spin('loading-migration');
-      AppsService.add(self.appName, self.appTitle)
-        .then(function (data) {
-          createDB(self.appName);
-        });
+      if (validate(self.parseUrl, self.parseSchema)) {
+        AppsService.add(self.appName, self.appTitle)
+          .then(function (data) {
+            createDB(self.appName);
+
+          });
+      } else {
+        usSpinnerService.stop('loading-migration');
+      }
     };
 
-    self.cancel = function(){
+    self.cancel = function () {
       $modalInstance.close();
     };
 
@@ -55,6 +60,45 @@
         usSpinnerService.stop('loading');
         $modalInstance.close({success: true});
       });
+    }
+
+    function validate(data, schema) {
+      if (validateData(data)) {
+        return validateSchema(schema);
+      }
+      return false;
+    }
+
+    function validateSchema(schema) {
+      try {
+        var schemaObject = JSON.parse(schema);
+      } catch (e) {
+        NotificationService.add('error', 'Parse schema should be a valid JSON');
+        return false;
+      }
+      if (!schemaObject.results) {
+        NotificationService.add('error', 'Schema should include "results" property');
+        return false;
+      }
+
+      var isContainValidObject = _.some(schemaObject.results, function (object) {
+        return object.className && object.fields;
+      });
+
+      if (!isContainValidObject) {
+        NotificationService.add('error', 'Schema should contain a valid object with "className" and "fields" properties');
+        return false;
+      }
+
+      return true;
+    }
+
+    function validateData(data) {
+      if (!data.startsWith("https://s3.amazonaws.com")) {
+        NotificationService.add('error', 'Parse data should start with "https://s3.amazonaws.com"');
+        return false;
+      }
+      return true;
     }
   }
 
