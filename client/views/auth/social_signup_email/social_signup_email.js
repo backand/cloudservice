5d08d8d8ad60ffc59b94915d6adb732499ac6309
Tamir Kfir <tamir.kk@gmail.com>
2016-03-26 20:50:19 +0300

    Created a modal to fill missing email when signing up with Twitter

---
+++ client/views/auth/social_signup_email/social_signup_email.js
@@ -0,0 +1,16 @@
+
+(function  () {
+
+  angular.module('backand')
+    .controller('SocialSignupEmailController', ['$modalInstance', SocialSignupEmailController]);
+
+  function SocialSignupEmailController($modalInstance) {
+    var self = this;
+
+    self.submit = function () {
+      // TODO send email to server
+      $modalInstance.close();
+    }
+  }
+
+}());
