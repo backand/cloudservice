b35383e1e79b38e32fc1b27133148b477abdf902
Matt Billock <mattbillock@mattbillock.com>
2017-01-27 13:06:39 -0800

    Navigation pages for frameworks added, need to add content to finish off

--- client/common/services/starter_app_service.js
+++ client/common/services/starter_app_service.js
@@ -46,14 +46,19 @@
           description: "An awesome project"
         },
         {
-          name: "To Do",
+          name: "To Do with Users",
           image: "assets/images/app_templates/to_do.png",
-          description: "An awesome project"
+          description: "This starter demonstrates using our authentication and sign-up systems to work with a To-Do List app"
         },
         {
-          name: "Back& Store",
+          name: "Back& CRUD",
           image: "assets/images/app_templates/backand_store.png",
-          description: "An awesome project"
+          description: "This starter app demonstrates building a model with Create, Read, Update, and Delete functionality"
+        },
+        {
+          name: "Back& Payments",
+          image: "assets/images/app_templates/to_do.png",
+          description: "This starter demonstrates connecting Back& application to a payment processor"
         }
       ];
       optionsHash[ 'Angular 2' ] = [{
@@ -68,36 +73,38 @@
         description: "An awesome project"
       },
       {
-        name: "To Do",
+        name: "Back& Users",
         image: "assets/images/app_templates/to_do.png",
-        description: "An awesome project"
+        description: "This starter for Angular 2 demonstrates using our authentication and sign-up systems"
       },
       {
-        name: "Back& Store",
+        name: "Back& Users and CRUD",
         image: "assets/images/app_templates/backand_store.png",
-        description: "An awesome project"
+        description: "This starter app demonstrates building an app using a basic CRUD model and user authentication, on top of Angular 2"
       }];
-      optionsHash[ 'Ionic' ] = [{
-        name: "Existing Project",
-        image: "assets/images/app_templates/blank_project.png",
-        description: "An awesome project",
-        images: ['assets/images/postgresql.jpg', 'assets/images/mysql.png']
-      },
-      {
-        name: "Blank Project",
-        image: "assets/images/app_templates/seed_project.png",
-        description: "An awesome project"
-      },
-      {
-        name: "To Do",
-        image: "assets/images/app_templates/to_do.png",
-        description: "An awesome project"
-      },
-      {
-        name: "Back& Store",
-        image: "assets/images/app_templates/backand_store.png",
-        description: "An awesome project"
-      }];
+      optionsHash[ 'Ionic' ] = [
+        {
+          name: "Existing Project",
+          image: "assets/images/app_templates/blank_project.png",
+          description: "An awesome project",
+          images: ['assets/images/postgresql.jpg', 'assets/images/mysql.png']
+        },
+        {
+          name: "Blank Project",
+          image: "assets/images/app_templates/seed_project.png",
+          description: "An awesome project"
+        },
+        {
+          name: "Ionic Starter with Social",
+          image: "assets/images/app_templates/to_do.png",
+          description: "This starter demonstrates using our social media authentication capabilities"
+        },
+        {
+          name: "Ionic Chat Starter",
+          image: "assets/images/app_templates/backand_store.png",
+          description: "This starter app demonstrates building a chat app using sockets through Backand's SDK"
+        }
+      ];
       optionsHash[ 'Ionic 2' ] = [{
         name: "Existing Project",
         image: "assets/images/app_templates/blank_project.png",
@@ -110,58 +117,36 @@
         description: "An awesome project"
       },
       {
-        name: "To Do",
+        name: "Ionic 2 Back& Starter",
         image: "assets/images/app_templates/to_do.png",
-        description: "An awesome project"
-      },
-      {
-        name: "Back& Store",
-        image: "assets/images/app_templates/backand_store.png",
-        description: "An awesome project"
+        description: "This starter demonstrates a simple integration with Back& using a lightweight 'ToDo' list management app"
       }];
-      optionsHash[ 'React' ] = [{
-        name: "Existing Project",
-        image: "assets/images/app_templates/blank_project.png",
-        description: "An awesome project",
-        images: ['assets/images/postgresql.jpg', 'assets/images/mysql.png']
-      },
-      {
-        name: "Blank Project",
-        image: "assets/images/app_templates/seed_project.png",
-        description: "An awesome project"
-      },
-      {
-        name: "To Do",
-        image: "assets/images/app_templates/to_do.png",
-        description: "An awesome project"
-      },
-      {
-        name: "Back& Store",
-        image: "assets/images/app_templates/backand_store.png",
-        description: "An awesome project"
-      }];
-      optionsHash[ 'React Native' ] = [{
-        name: "Existing Project",
-        image: "assets/images/app_templates/blank_project.png",
-        description: "An awesome project",
-        images: ['assets/images/postgresql.jpg', 'assets/images/mysql.png']
-      },
-      {
-        name: "Blank Project",
-        image: "assets/images/app_templates/seed_project.png",
-        description: "An awesome project"
-      },
-      {
-        name: "To Do",
-        image: "assets/images/app_templates/to_do.png",
-        description: "An awesome project"
-      },
-      {
-        name: "Back& Store",
-        image: "assets/images/app_templates/backand_store.png",
-        description: "An awesome project"
-      }];
-      console.log(optionsHash[platformName]);
+      optionsHash[ 'React' ] = [
+        {
+          name: "Existing Project",
+          image: "assets/images/app_templates/blank_project.png",
+          description: "An awesome project",
+          images: ['assets/images/postgresql.jpg', 'assets/images/mysql.png']
+        },
+        {
+          name: "Blank Project",
+          image: "assets/images/app_templates/seed_project.png",
+          description: "An awesome project"
+        }
+      ];
+      optionsHash[ 'React Native' ] = [
+        {
+          name: "Existing Project",
+          image: "assets/images/app_templates/blank_project.png",
+          description: "An awesome project",
+          images: ['assets/images/postgresql.jpg', 'assets/images/mysql.png']
+        },
+        {
+          name: "Blank Project",
+          image: "assets/images/app_templates/seed_project.png",
+          description: "An awesome project"
+        }
+      ];
       return optionsHash[platformName];
     };
   }
