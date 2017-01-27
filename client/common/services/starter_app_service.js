80effe35838a2f320aed947e4965094533945b0b
Matt Billock <mattbillock@mattbillock.com>
2017-01-27 13:29:10 -0800

    Updated descriptions

--- client/common/services/starter_app_service.js
+++ client/common/services/starter_app_service.js
@@ -14,36 +14,26 @@
         {
           name: "Existing Project",
           image: "assets/images/app_templates/blank_project.png",
-          description: "An awesome project",
+          description: "Integrate Back& with an existing web app",
           images: ['assets/images/postgresql.jpg', 'assets/images/mysql.png']
         },
         {
           name: "Blank Project",
           image: "assets/images/app_templates/seed_project.png",
-          description: "An awesome project"
-        },
-        {
-          name: "To Do",
-          image: "assets/images/app_templates/to_do.png",
-          description: "An awesome project"
-        },
-        {
-          name: "Back& Store",
-          image: "assets/images/app_templates/backand_store.png",
-          description: "An awesome project"
+          description: "Start a new web app with Back&"
         }
       ];
       optionsHash[ 'AngularJS' ] = [
         {
           name: "Existing Project",
           image: "assets/images/app_templates/blank_project.png",
-          description: "An awesome project",
+          description: "Integrate Back& with an existing Angular web app",
           images: ['assets/images/postgresql.jpg', 'assets/images/mysql.png']
         },
         {
           name: "Blank Project",
           image: "assets/images/app_templates/seed_project.png",
-          description: "An awesome project"
+          description: "Start a new Angular web app with Back&"
         },
         {
           name: "To Do with Users",
@@ -64,13 +54,13 @@
       optionsHash[ 'Angular 2' ] = [{
         name: "Existing Project",
         image: "assets/images/app_templates/blank_project.png",
-        description: "An awesome project",
+        description: "Integrate Back& with an existing Angular 2 web app",
         images: ['assets/images/postgresql.jpg', 'assets/images/mysql.png']
       },
       {
         name: "Blank Project",
-        image: "assets/images/app_templates/seed_project.png",
-        description: "An awesome project"
+        image: "assets/images/app_templates/seed_project.png",,
+        description: "Start a new Angular 2 web app with Back&"
       },
       {
         name: "Back& Users",
@@ -86,13 +76,13 @@
         {
           name: "Existing Project",
           image: "assets/images/app_templates/blank_project.png",
-          description: "An awesome project",
+          description: "Integrate Back& with an existing Ionic app",
           images: ['assets/images/postgresql.jpg', 'assets/images/mysql.png']
         },
         {
           name: "Blank Project",
-          image: "assets/images/app_templates/seed_project.png",
-          description: "An awesome project"
+          image: "assets/images/app_templates/seed_project.png",,
+          description: "Start a new Ionic app with Back&"
         },
         {
           name: "Ionic Starter with Social",
@@ -108,13 +98,13 @@
       optionsHash[ 'Ionic 2' ] = [{
         name: "Existing Project",
         image: "assets/images/app_templates/blank_project.png",
-        description: "An awesome project",
+        description: "Integrate Back& with an existing Ionic 2 web app",
         images: ['assets/images/postgresql.jpg', 'assets/images/mysql.png']
       },
       {
         name: "Blank Project",
-        image: "assets/images/app_templates/seed_project.png",
-        description: "An awesome project"
+        image: "assets/images/app_templates/seed_project.png",,
+        description: "Start a new Ionic 2 app with Back&"
       },
       {
         name: "Ionic 2 Back& Starter",
@@ -125,26 +115,26 @@
         {
           name: "Existing Project",
           image: "assets/images/app_templates/blank_project.png",
-          description: "An awesome project",
+          description: "Integrate Back& with an existing React web app",
           images: ['assets/images/postgresql.jpg', 'assets/images/mysql.png']
         },
         {
           name: "Blank Project",
-          image: "assets/images/app_templates/seed_project.png",
-          description: "An awesome project"
+          image: "assets/images/app_templates/seed_project.png",,
+          description: "Start a new React web app with Back&"
         }
       ];
       optionsHash[ 'React Native' ] = [
         {
           name: "Existing Project",
           image: "assets/images/app_templates/blank_project.png",
-          description: "An awesome project",
+          description: "Integrate Back& with an existing React Native app",
           images: ['assets/images/postgresql.jpg', 'assets/images/mysql.png']
         },
         {
           name: "Blank Project",
-          image: "assets/images/app_templates/seed_project.png",
-          description: "An awesome project"
+          image: "assets/images/app_templates/seed_project.png",,
+          description: "Start a new React Native app with Back&"
         }
       ];
       return optionsHash[platformName];
