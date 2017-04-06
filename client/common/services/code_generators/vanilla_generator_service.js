9c130e14ba91896168b857c0a55461001b6fd09a
Ran Cohen <rannn505@outlook.com>
2017-04-07 02:49:27 +0300

    add fn + fix params in code-gen

--- client/common/services/code_generators/vanilla_generator_service.js
+++ client/common/services/code_generators/vanilla_generator_service.js
@@ -10,66 +10,101 @@
       object: {
         GET: [{
           name: 'getList',
-          parameters: ['object', 'params'],
+          params: ['object', 'options'],
           details: {}
         }, {
           name: 'getOne',
-          parameters: ['object', 'id', 'param'],
+          params: ['object', 'id', 'options'],
           details: {}
         }],
         POST: {
           name: 'create',
-          parameters: ['object', 'data', 'params'],
+          params: ['object', 'data', 'options', 'parameters'],
           details: {}
         },
         PUT: {
           name: 'update',
-          parameters: ['object', 'id', 'data', 'params'],
+          params: ['object', 'id', 'data', 'options', 'parameters'],
           details: {}
         },
         DELETE: {
           name: 'remove',
-          parameters: ['object', 'id'],
+          params: ['object', 'id', 'parameters'],
           details: {}
         }
       },
       action: {
         GET: {
           name: 'get',
-          parameters: ['object', 'action', 'params'],
+          params: ['object', 'action', 'parameters'],
           details: {}
         },
         POST: {
           name: 'post',
-          parameters: ['object', 'action', 'data', 'params'],
+          params: ['object', 'action', 'data', 'parameters'],
           details: {}
         }
       },
       query: {
         GET: {
           name: 'get',
-          parameters: ['name', 'params'],
+          params: ['name', 'parameters'],
           details: {}
         },
         POST: {
           name: 'post',
-          parameters: ['name', 'data', 'params'],
+          params: ['name', 'parameters'],
+          details: {}
+        }
+      },
+      "function": {
+        GET: {
+          name: 'get',
+          params: ['name', 'parameters'],
+          details: {}
+        },
+        POST: {
+          name: 'post',
+          params: ['name', 'data', 'parameters'],
           details: {}
         }
       }
     };
-    var extractionRules = {
-      name: ['url'],
-      object: ['url', '>?'],
-      action: ['url', '<='],
-      id: ['url', '>?'],
-      data: ['obj'],
-      params: ['obj']
+    var extractionMethods = {
+      name: function(splitUrl, httpObject) {
+        var param = splitUrl.pop();
+        if(param.indexOf('?') !== -1) {
+          param = param.slice(0, param.indexOf('?'));
+        }
+        return param;
+      },
+      object: function(splitUrl, httpObject) {
+        var param = splitUrl.pop();
+        return param.slice(0, param.indexOf('?'));
+      },
+      action: function(splitUrl, httpObject) {
+        var param = splitUrl.pop();
+        return param.slice(param.indexOf('=') + 1);
+      },
+      id: function(splitUrl, httpObject) {
+        var param = splitUrl.pop();
+        return param.slice(0, param.indexOf('?'));
+      },
+      data: function(splitUrl, httpObject) {
+        return httpObject.data;
+      },
+      options: function(splitUrl, httpObject) {
+        httpObject.params && httpObject.params.parameters && delete httpObject.params.parameters;
+        return httpObject.params;
+      },
+      parameters: function(splitUrl, httpObject) {
+        return httpObject.params && httpObject.params.parameters ? httpObject.params.parameters : undefined;
+      }
     }
 
 
     self.generateCode = function (httpObject, opts) {
-      // console.log(httpObject);
+      console.log(httpObject);
       // console.log(opts);
       httpObject = angular.fromJson(httpObject);
 
@@ -87,13 +122,18 @@
 
       if (splitUrl.indexOf('action') !== -1) {
         chosen = dictionary['action'][httpObject.method];
-        __extractFunctionDetails__(splitUrl, httpObject);
+        __extractFunctionParams__(splitUrl, httpObject);
         return '.object.action.' + chosen.name + __fixParameters__()
       }
       else if (splitUrl.indexOf('query') !== -1) {
         chosen = dictionary['query'][httpObject.method];
-        __extractFunctionDetails__(splitUrl, httpObject);
+        __extractFunctionParams__(splitUrl, httpObject);
         return '.query.' + chosen.name + __fixParameters__()
+      }
+      else if (splitUrl.indexOf('function') !== -1) {
+        chosen = dictionary['function'][httpObject.method];
+        __extractFunctionParams__(splitUrl, httpObject);
+        return '.fn.' + chosen.name + __fixParameters__()
       }
       else {
         if(httpObject.method === 'GET') {
@@ -104,52 +144,34 @@
         else {
           chosen = dictionary['object'][httpObject.method];
         }
-        __extractFunctionDetails__(splitUrl, httpObject);
+        __extractFunctionParams__(splitUrl, httpObject);
         return '.object.' + chosen.name + __fixParameters__()
       }
     }
 
-    function __extractFunctionDetails__ (splitUrl, httpObject) {
-      for (var i = chosen.parameters.length - 1; i > -1 ; i--) {
-        var _detail;
-        for (var j = 0; j < extractionRules[chosen.parameters[i]].length; j++) {
-          var _rule = extractionRules[chosen.parameters[i]][j];
-          if(_rule === 'obj') {
-            _detail = httpObject[chosen.parameters[i]];
-          }
-          else if (_rule === 'url') {
-            _detail = splitUrl.pop();
-          }
-          else {
-            var _pos = _detail.indexOf(_rule[1]);
-            if (_pos !== -1) {
-              _rule[0] === '>' ?
-              _detail = _detail.slice(0, _pos) :
-              _detail = _detail.slice(_pos + 1);
-            }
-          }
-          chosen.details[chosen.parameters[i]] = _detail;
-        }
+    function __extractFunctionParams__ (splitUrl, httpObject) {
+      // loop end to start over params array;
+      for (var i = chosen.params.length - 1; i > -1 ; i--) {
+        var _cur = chosen.params[i];
+        // call current param extraction method;
+        chosen.details[_cur] = extractionMethods[_cur](splitUrl, httpObject);
       }
     }
 
     function __fixParameters__ () {
       var _parameters = '(';
-
-      for (var i = 0; i < chosen.parameters.length; i++) {
-        _parameters = _parameters.concat(angular.toJson(chosen.details[chosen.parameters[i]], true) || '');
-        if (chosen.details[chosen.parameters[i + 1]]) {
+      for (var i = 0; i < chosen.params.length; i++) {
+        _parameters = _parameters.concat(angular.toJson(chosen.details[chosen.params[i]], true) || '');
+        if (chosen.details[chosen.params[i + 1]]) {
           _parameters = _parameters.concat(', ');
         }
       }
-
       return _parameters.concat(')');
     }
 
     function __generatePromiseString__ (es6) {
       var _then = es6 ? 'data => { }' : 'function(data) { }';
       var _catch = es6 ? 'error => { }' : 'function(error) { }';
-
       return '\n.then(' + _then + ')\n' +
         '.catch(' + _catch + ')'
     }
